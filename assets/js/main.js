df0393c2e06a234798d044860c93709cb00d8fcb
canyetgin <cannyetgin@gmail.com>
2022-12-10 08:01:03 +0300

    about section

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,12 @@ navClose.addEventListener('click', () => {
   navMenu.classList.remove('show-menu')
 })
 
+const scrollHeader = () => {
+  const header = document.getElementById('header')
+  this.scrollY >= 50
+    ? header.classList.add('bg-header')
+    : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
 let rellax = new Rellax('.parallax')
